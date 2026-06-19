7706afe69dbf5e8438004467f97e85be6f4b0645
arshil692-prog <muktars692@gmail.com>
2026-06-19 09:53:06 +0530

    signal

--- file2.py
+++ file2.py
@@ -1,1 +1,1 @@
-print ("arshil")
+print("Hello, World!")
---
+++ world.txt/file1.py
@@ -0,0 +1,8 @@
+signal = green 
+if signal == "red":
+    print("stop")
+    elif signal == "yellow":
+        print("wait")
+        elif signal == "green":
+            print("go")
+            
